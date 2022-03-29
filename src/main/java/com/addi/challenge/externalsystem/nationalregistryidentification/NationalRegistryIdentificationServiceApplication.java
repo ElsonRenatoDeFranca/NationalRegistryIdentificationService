99ad0943f97e823f9faeb05d34a3d3145dfd10b8
ElsonRenatoDeFranca <elson.renato.de.franca@gmail.com>
2022-03-28 23:17:36 -0300

    Feature-001 - project structure

---
+++ src/main/java/com/addi/challenge/externalsystem/nationalregistryidentification/NationalRegistryIdentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.addi.challenge.externalsystem.nationalregistryidentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalRegistryIdentificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalRegistryIdentificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addi/challenge/externalsystem/nationalregistryidentification/NationalregistryidentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addi.challenge.externalsystem.nationalregistryidentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalregistryidentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
